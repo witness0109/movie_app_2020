19d8d25aca3671f6837d6ac4c4bed773b7e77b62
witness0109 <witness0109@gmail.com>
2020-07-01 23:49:42 +0900

    0701

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+class App extends React.Component{
+  state = {
+      count : 0,
+  }
+
+  add = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  minus = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The number is: {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
   );
+  }
+
 }
+
+
 
 export default App;
