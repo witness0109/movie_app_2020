9a8b30e1054111c033c04565568c52f14c115f9a
witness0109 <witness0109@gmail.com>
2020-07-02 21:10:40 +0900

    0702-1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React from 'react';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log('hello');
+  }
+
+
   state = {
       count : 0,
   }
 
   add = () => {
-    this.setState({ count: this.state.count + 1 })
+    this.setState(current => ({
+      count: current.count +1,
+
+    }));
   }
 
   minus = () => {
-    this.setState({ count: this.state.count - 1 })
+    this.setState(current => ({
+      count: current.count -1,
+
+    }));
   }
 
+  componentDidMount(){
+    console.log('component rendered');
+  }
+
+  componentDidUpdate(){
+    console.log('I just updated');
+  }
+componentWillUnmount(){
+  console.log('Goodbye, cruel world');
+}
+
+
   render() {
+    console.log("I'm render");
   return (
     <div>
       <h1>The number is: {this.state.count}</h1>
